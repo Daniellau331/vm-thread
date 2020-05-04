268c66b58b0f14029f95c61ec9c0a2a4df309968
Daniellau331 <daniellau331@gmail.com>
2020-05-03 21:55:40 -0700

    update

--- README.md
+++ README.md
@@ -13,3 +13,4 @@
 - status
 - Prio
 - MachineContext
+
--- src/VirtualMachine.cpp
+++ src/VirtualMachine.cpp
@@ -21,113 +21,102 @@ extern "C"
         TVMThreadEntry entry;
         SMachineContext context;
         TVMThreadIDRef tid;
-        void  *param;
+        void *param;
+        void * stackAddr;
     };
 
     class AllThreadInfo
     {
     public:
-        vector<TCB*> allThreadList;
-        vector<TCB*> readyThread;
+        // TCB* cur_thread;
+        TVMThreadID CurThreadID;
+        vector<TCB *> allThreadList;
+        queue<TCB *> readyThread;
     } allThreadInfo;
 
-    // keep track the current number of thread
-    volatile int threadNum = 0;
 
-    // **************Globle variable **************
+    //Global Variable 
+    volatile int threadNum;
     volatile int Globle_tick;
-
-    //When a thread is ready, it should be added to the queues
     queue<TCB> ReadyThreadList;
 
     //May have a list of waiting threads, those that are waiting on sleep, or for a file operation
 
-    TVMStatus VMStart(int tickms, int argc, char *argv[]);
+    //Function declaration
     TVMMainEntry VMLoadModule(const char *module);
-    void MachineInitialize(void);
-    void MachineEnableSignals(void);
-    void MachineTerminate(void);
     void VMUnloadModule(void);
-    TVMStatus VMFileWrite(int filedescriptor, void *data, int *length);
-    typedef void (*TMachineFileCallback)(void *calldata, int result);
-    TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsize, TVMThreadPriority prio, TVMThreadIDRef tid);
     void scheduler();
-    TVMStatus VMThreadState(TVMThreadID thread, TVMThreadStateRef state);
-    TVMStatus VMThreadActivate(TVMThreadID thread);
+    void printThreadList();
+    void alarmCallback(void *calldata);
+    void skeleton(void* param);
 
-    //need a skeleton function to be the initial entry point for the thread
 
     // Callback function for the MachineRequestAlarm
     void alarmCallback(void *calldata)
     {
         Globle_tick--;
-        cout << Globle_tick << endl;
     }
 
-    //should start with VMStart and VMFileWrite
+     // use this funciton to call the threads entry and ThreadTerminate in case the
+    // thread returns from its entry function
+    void skeleton(void* param)
+    {
+        cout<<"skeleton"<<endl;
+        TCB* thread = (TCB*) param;
+        thread->entry(thread->param);
+
+        VMThreadTerminate(allThreadInfo.CurThreadID);
+    }
+
+    /*
+    Descritption:
+    VMStart() starts the virtual machine by loading the module specified by argv[0]. 
+    The argc and argv are passed directly into the VMMain() function that exists in 
+    the loaded module. The time in milliseconds of the virtual machine tick is specified by the tickms parameter.
+    */
     TVMStatus VMStart(int tickms, int argc, char *argv[])
     {
-        cout << "********VMStart********" << endl;
 
-        // TCB for the main thread
-        TCB *mainThread = new TCB();
-        mainThread->threadID = threadNum++;
-        mainThread->priority = VM_THREAD_PRIORITY_NORMAL;
-        mainThread->state = VM_THREAD_STATE_RUNNING;
-        mainThread->memorySize = 0;
-        allThreadInfo.allThreadList.push_back(mainThread);
 
-        //Create an idle thread, when all other thread are blocked
-        TCB *idleThread = new TCB();
-        idleThread->state = VM_THREAD_STATE_READY;
-        idleThread->priority = VM_THREAD_PRIORITY_LOW;
-        idleThread->threadID = threadNum++;
-        allThreadInfo.allThreadList.push_back(idleThread);
+        TVMThreadID VMThreadID, IdleThreadID;
+        VMThreadCreate(skeleton, NULL, 0x100000, VM_THREAD_PRIORITY_NORMAL, &VMThreadID);
+        allThreadInfo.allThreadList[0]->state = VM_THREAD_STATE_RUNNING;
+        VMThreadCreate(skeleton,NULL,0x100000,VM_THREAD_PRIORITY_LOW,&IdleThreadID);
 
-        // 1.load the module with VMLoad that is specifiec by argv[0]
+   
         TVMMainEntry mainEntry = VMLoadModule(argv[0]);
 
-        // 2. Initialize the machine with MachineInitialize
         MachineInitialize();
-
-        TMachineAlarmCallback callback = alarmCallback;
-        MachineRequestAlarm(tickms * 1000, callback, NULL);
-
-        // 3. Enable signials with MachineEnablesSignals
+        MachineRequestAlarm(tickms * 1000, alarmCallback, NULL);
         MachineEnableSignals();
 
-        // 4. Call the VMMain entry point
         if (mainEntry)
             mainEntry(argc, argv);
         else
             return VM_STATUS_FAILURE;
 
-        // 5. Terminate the machine with MachineTerminate
         MachineTerminate();
-
-        // 6. Unload the module with VMUnloadModule
         VMUnloadModule();
-
-        delete idleThread;
-
-        // 7. Return form VMStart
         return VM_STATUS_SUCCESS;
     }
 
     // It creates a thread in the VM.
     TVMStatus VMThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize memsize, TVMThreadPriority prio, TVMThreadIDRef tid)
     {
-        if (!entry || !tid)
-            return VM_STATUS_ERROR_INVALID_PARAMETER;
+        if (!entry || !tid) return VM_STATUS_ERROR_INVALID_PARAMETER;
+
 
         TCB *thread = new TCB;
+        thread->threadID = threadNum++;
         thread->entry = entry;
         thread->param = param;
         thread->memorySize = memsize;
         thread->priority = prio;
         thread->param = param;
-        // thread->threadID = *tid++;
-        thread->threadID = threadNum++;
+        thread->state = VM_THREAD_STATE_DEAD;
+        *tid = thread->threadID;
+        allThreadInfo.allThreadList.push_back(thread);
+        // printThreadList();
 
         return VM_STATUS_SUCCESS;
     }
@@ -143,8 +132,6 @@ extern "C"
         if (!state)
             return VM_STATUS_ERROR_INVALID_PARAMETER;
 
-        //there should be a data sturcture to hold all thread, and we need to iterate the list
-        //all threads are stored in allThreadInfo.
         for (int i = 0; i < allThreadInfo.allThreadList.size(); i++)
         {
             if (allThreadInfo.allThreadList[i]->threadID == thread)
@@ -194,20 +181,47 @@ extern "C"
         if (tick == VM_TIMEOUT_INFINITE)
             return VM_STATUS_ERROR_INVALID_PARAMETER;
 
+        // Cannot use global varible to keep track of the tick
+        // There might be more than two sleeping thread at the same time
         Globle_tick = tick;
-        while (Globle_tick != 0)
-            ;
+        //need to add a loop here, we don't want the code exits before the Globle_tick became 0
+        while (Globle_tick != 0);
 
-        cout << Globle_tick;
+        //When this thread is sleeping, we need to schedue other thread
+        // scheduler();
 
         return VM_STATUS_SUCCESS;
     }
 
-    //MachineContextCreate() create a context that will enter here
-    void entry(void*){
-        cout<<"entry";
+    /*
+    Description:
+    VMThreadTerminate() terminates the thread specified by thread parameter in the virtual machine. 
+    After termination the thread enters the state VM_THREAD_STATE_DEAD. The termination of a thread 
+    can trigger another thread to be scheduled.
+    */
+    TVMStatus VMThreadTerminate(TVMThreadID thread)
+    {
+        if (!thread)
+            return VM_STATUS_ERROR_INVALID_ID;
+        bool found = false;
+
+        for (int i = 0; i < allThreadInfo.allThreadList.size(); i++)
+        {
+            if (thread == allThreadInfo.allThreadList[i]->threadID)
+            {
+                found = true;
+                if (allThreadInfo.allThreadList[i]->state == VM_THREAD_STATE_DEAD)
+                    return VM_STATUS_ERROR_INVALID_STATE;
+                allThreadInfo.allThreadList[i]->state = VM_THREAD_STATE_DEAD;
+                //The termination of a thread can trigger another thread to be scheduled.
+            }
+        }
+        if (!found)
+            return VM_STATUS_ERROR_INVALID_ID;
+        return VM_STATUS_SUCCESS;
     }
 
+   
 
     /*
     Description:
@@ -218,31 +232,21 @@ extern "C"
     TVMStatus VMThreadActivate(TVMThreadID thread)
     {
         bool found = false;
+
         for (int i = 0; i < allThreadInfo.allThreadList.size(); i++)
         {
             if (allThreadInfo.allThreadList[i]->threadID == thread)
             {
                 found = true;
-                if (allThreadInfo.allThreadList[i]->state != VM_THREAD_STATE_DEAD)
-                    return VM_STATUS_ERROR_INVALID_STATE;
-                // after avtivation, the state of the thread will become ready
-                allThreadInfo.allThreadList[i]->state = VM_THREAD_STATE_READY;
-                // initialize the SMachineContext
-                SMachineContextRef mcntxref = new SMachineContext;
+                TCB* thread = allThreadInfo.allThreadList[i];
+                if (thread->state != VM_THREAD_STATE_DEAD) return VM_STATUS_ERROR_INVALID_STATE;
 
-                /*
-                Description:
-                    MachineContextCreate() creates a context that will enter in the function specified by entry
-                     and passing it the parameter param. The contexts stack of size stacksize must be specified 
-                     by the stackaddr parameter. The newly created context will be stored in the mcntxref parameter, 
-                     this context can be used in subsequent calls to MachineContextRestore(), or MachineContextSwitch().
-                */
-                void* stackaddr = (void *)malloc(allThreadInfo.allThreadList[i]->memorySize);
-                MachineContextCreate(mcntxref,
-                                          entry, allThreadInfo.allThreadList[i]->param, stackaddr, allThreadInfo.allThreadList[i]->memorySize);
-                allThreadInfo.allThreadList[i]->context = *mcntxref;
-                allThreadInfo.readyThread.push_back(allThreadInfo.allThreadList[i]);
-            
+
+                thread->stackAddr = (void *)malloc(thread->memorySize);
+                cout<<"MCC()"<<endl;
+                MachineContextCreate(&thread->context,skeleton, thread, thread->stackAddr, allThreadInfo.allThreadList[i]->memorySize);
+                allThreadInfo.allThreadList[i]->state = VM_THREAD_STATE_READY;
+                allThreadInfo.readyThread.push(thread);
             }
         }
 
@@ -251,7 +255,21 @@ extern "C"
         return VM_STATUS_SUCCESS;
     }
 
+    // Determine who should run next from the ready queue
     void scheduler()
     {
+        SMachineContextRef mcntxold, mcntxnew;
+        //old machine context is the current thread's machine context
+        mcntxold = &(allThreadInfo.allThreadList[allThreadInfo.CurThreadID]->context);
+        //Get the new machine context from the ready list
+        mcntxnew = &(allThreadInfo.readyThread.front()->context);
+        allThreadInfo.CurThreadID = allThreadInfo.readyThread.front()->threadID;
+        MachineContextSwitch(mcntxold, mcntxnew);
+    }
+
+    void printThreadList(){
+        for(auto item:allThreadInfo.allThreadList){
+            cout<<"Thread ID: "<<item->threadID<<" state: "<<item->state<<endl;
+        }
     }
 }
